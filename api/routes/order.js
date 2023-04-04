4111d47e8995a334c2475f9b35cfebdd26288ec3
Neilly28 <neilpilarca@gmail.com>
2023-04-04 22:43:47 +0200

    add cart and order CRUD operations

--- api/routes/order.js
+++ api/routes/order.js
@@ -1,3 +1,69 @@
 const router = require("express").Router();
+const {
+  verifyToken,
+  verifyTokenAndAuth,
+  verifyTokenAndAdmin,
+} = require("./verifyToken");
+const CryptoJS = require("crypto-js");
+const Order = require("../models/Order");
+
+// CREATE ORDER
+router.post("/", verifyToken, async (req, res) => {
+  const newOrder = new Order(req.body);
+  try {
+    const savedOrder = await newOrder.save();
+    res.status(200).json(savedOrder);
+  } catch (error) {
+    res.status(500).json(error);
+  }
+});
+
+// UPDATE ORDER
+router.put("/:id", verifyTokenAndAdmin, async (req, res) => {
+  try {
+    const updatedOrder = await Order.findByIdAndUpdate(
+      req.params.id,
+      {
+        $set: req.body,
+      },
+      { new: true }
+    );
+    res.status(200).json(updatedOrder);
+  } catch (error) {
+    res.status(500).json(error);
+  }
+});
+
+// DELETE ORDER
+router.delete("/:id", verifyTokenAndAdmin, async (req, res) => {
+  try {
+    await Order.findByIdAndDelete(req.params.id);
+    res.status(200).json("order has been deleted");
+  } catch (error) {
+    res.status(500).json(error);
+  }
+});
+
+// GET USER ORDERS
+router.get("/find/:userId", verifyTokenAndAuth, async (req, res) => {
+  try {
+    const orders = await Order.find({ userId: req.params.id });
+    res.status(200).json(orders);
+  } catch (error) {
+    res.status(500).json(error);
+  }
+});
+
+// GET ALL ORDERS
+router.get("/", verifyTokenAndAdmin, async (req, res) => {
+  try {
+    const orders = await Order.find();
+    res.status(200).json(orders);
+  } catch (error) {
+    res.status(500).json(error);
+  }
+});
+
+// GET MONTHLY INCOME
 
 module.exports = router;
