9da28fab20271dc7e172b4bade8160ea5a8cc6d0
Neilly28 <neilpilarca@gmail.com>
2023-04-07 22:23:45 +0200

    add react router

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,12 +2,39 @@ import Home from "./pages/Home";
 import Product from "./pages/Product";
 import ProductList from "./pages/ProductList";
 import Register from "./pages/Register";
+import Login from "./pages/Login";
+import Cart from "./pages/Cart";
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  Link,
+  Navigate,
+} from "react-router-dom";
 
 function App() {
+  const user = true;
+
   return (
-    <div className="App">
-      <Home />
-    </div>
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/products/:category" element={<ProductList />} />
+        <Route path="/product/:id" element={<Product />} />
+        <Route path="/cart" element={<Cart />} />
+        <Route
+          path="/login"
+          element={!user ? <Login /> : <Navigate to="/" />}
+        />
+        <Route
+          path="/register"
+          element={!user ? <Register /> : <Navigate to="/" />}
+        />
+      </Routes>
+    </Router>
+    // <div className="App">
+    //   <Home />
+    // </div>
   );
 }
 
