67e7e659cce33c126a8d7ead9948cf14f27a5b74
Neilly28 <neilpilarca@gmail.com>
2023-04-10 19:40:10 +0200

    add user login redux

--- client/src/redux/apiCalls.js
+++ client/src/redux/apiCalls.js
@@ -1,14 +1,21 @@
 import { loginFailure, loginStart, loginSuccess } from "./userSlice";
+import axios from "axios";
 
 export const login = async (dispatch, user) => {
+  // dispatch loginStart first
   dispatch(loginStart());
+
+  // try-catch block needed for api call
   try {
     const response = await axios.post(
       "http://localhost:5000/api/auth/login",
       user
     );
-    dispatch(loginSuccess());
+
+    // dispatch loginSuccess if OK
+    dispatch(loginSuccess(response.data));
   } catch (error) {
+    // dispatch loginFailure if there is Error
     dispatch(loginFailure());
   }
 };
